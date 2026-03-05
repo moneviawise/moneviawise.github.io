01a81425d2dab2c950bcf6915c3ecf63ecfd0497
Todd Long <hartforddavid@yahoo.com>
2026-03-04 19:25:47 -0500

    Update site add email collection and Polices

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -4,6 +4,24 @@ async function loadPartial(id, url) {
 
   const res = await fetch(url);
   el.innerHTML = await res.text();
+
+    // Run footer script after loading
+  if (id === "site-footer") {
+    const year = document.getElementById("year");
+    if (year) year.textContent = new Date().getFullYear();
+  }
+
+    // Active navigation link
+  if (id === "site-header") {
+    const links = document.querySelectorAll(".nav-links a");
+    const currentPage = window.location.pathname.split("/").pop();
+
+    links.forEach(link => {
+      if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active");
+      }
+    });
+  }
 }
 
 loadPartial("site-header", "/partials/header.html");
